a2b7299fe96ebc345faed29b3ade95a52e863cb4
LazurcaDaniel <daniel.lazurca@info.uaic.ro>
2023-02-25 18:40:18 +0200

    Prob 1 modificata pentru nr mari

--- Lab 1/prob1.cpp
+++ Lab 1/prob1.cpp
@@ -1,30 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
 #include <cstring>
 using namespace std;
-int main() 
+void add_big_numbers(char s1[], char s2[], char rez[])
+{
+    char n1[100], n2[100];
+    strcpy(n1, s1);
+    strcpy(n2, s2);
+    int l1 = strlen(n1);
+    int l2 = strlen(n2);
+    int nr_cifre = 0;
+    int carry = 0;
+    if (l1 < l2)
+    {
+        for (int i = l2 - 1; i >= l2 - l1; i--)
+            n1[i] = n1[i - (l2 - l1)];
+        for (int i = 0; i < l2 - l1; i++)
+            n1[i] = '0';
+        l1 = l2;
+        n1[l1] = '\0';
+    }
+    else if (l2 < l1)
+    {
+        for (int i = l1 - 1; i >= l1 - l2; i--)
+            n2[i] = n2[i - (l1 - l2)];
+        for (int i = 0; i < l1 - l2; i++)
+            n2[i] = '0';
+        l2 = l1;
+        n2[l2] = '\0';
+    }
+    while (l1 > 0 && l2 > 0)
+    {
+        int nr = (n1[l1 - 1] - '0') + (n2[l2 - 1] - '0') + carry;
+        rez[nr_cifre] = (nr % 10) + '0';
+        carry = nr / 10;
+        nr_cifre++;
+        l1--;
+        l2--;
+    }
+    if (carry)
+        rez[nr_cifre++] = '1';
+    rez[nr_cifre] = '\0';
+    for (int i = 0; i < nr_cifre / 2; i++)
+        swap(rez[i], rez[nr_cifre - i - 1]);
+
+}
+int main()
 {
     FILE* fp;
-    long suma = 0;
-    if (fopen_s(&fp, "in.txt", "r") != 0)
+    if (fopen_s(&fp, "ini.txt", "r") != 0)
     {
         printf("Eroare. Nu am putut deschide fisierul\n");
     }
     else
     {
-        char s[100];
-        while (fgets(s, 100, fp) != NULL)
+
+        char s1[100], s2[100], buffer[1];
+        fgets(s1, 100, fp);
+        strcpy(s1 + strlen(s1) - 1, s1 + strlen(s1));
+        while (fgets(s2, 100, fp) != nullptr)
         {
-            int x = 0;
-            printf("%s\n", s);
-            for (int i = 0; i < strlen(s); i++)
-            {
-                if (s[i] != '\0')
-                    x = x * 10 + (s[i] - '0');
-            }
-            suma += x;
+            char rez[100];
+            strcpy(s2 + strlen(s2) - 1, s2 + strlen(s2));
+            add_big_numbers(s1, s2, rez);
+            //cout << rez << '\n' << '\n';
+            strcpy(s1, rez);
         }
-        printf("Suma numerelor este: %d", suma);
+        printf("Suma numerelor este: %s", s1);
     }
     return 0;
 }
